917322e1cef4dbda6e71d479d4929a1bc4e96ba2
rukonit <rukon-it@live.com>
2021-06-09 00:53:32 -0400

    added form functinality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,35 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted: boolean = false;
+
+
    suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    this.signUpForm.form.patchValue({userData: {
+      username: suggestedName
+    }})
   }
 
   // onSubmit(form: NgForm) {
@@ -18,6 +45,15 @@ export class AppComponent {
   // }
 onSubmit(){
 
-  console.log(this.signUpForm);
+  this.user.username = this.signUpForm.value.userData.username;
+  this.user.email = this.signUpForm.value.userData.email;
+  this.user.secretQuestion = this.signUpForm.value.secret;
+  this.user.answer = this.signUpForm.value.questionAnswer;
+  this.user.gender = this.signUpForm.value.gender;
+
+  this.submitted = true;
+
+  this.signUpForm.reset();
+
 }
 }
